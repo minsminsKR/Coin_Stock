2606d8f7e5d4943f560255b581f20bce10e742e2
Minsoo Kang <3382364@naver.com>
2024-01-10 00:14:56 +0900

    Third commit
    주식 항목 선택 가능.
    타이틀, 주식 이름에 맞게 변경

--- stock.py
+++ stock.py
@@ -4,15 +4,27 @@ import yfinance as yf
 import plotly.graph_objects as go
 from prophet import Prophet
 
-st.write('''
-# 삼성전자 주식 데이터
+# '005930.KS'(삼성 종목 코드)의 주식 데이터
+# https://finance.yahoo.com/quote/005930.KS?p=005930.KS
+## 주식정보 받아오기
+def get_krx_stock_codes():
+    url = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13'
+    df = pd.read_html(url, header=0, encoding='CP949')[0]
+    df.종목코드 = df.종목코드.map('{:06d}'.format) + '.KS'  
+    return df
+
+df = get_krx_stock_codes()
+
+selected_name = st.sidebar.selectbox('Name', df['회사명'])
+selected_code = df[df['회사명'] == selected_name]['종목코드'].values[0]
+########################################################################################
+
+st.write(f'''
+# {selected_name} 주식 데이터
 마감 가격과 거래량 시각화
 ''')
 
-# '005930.KS'(삼성 종목 코드)의 주식 데이터
-# https://finance.yahoo.com/quote/005930.KS?p=005930.KS
-# 두산 테스나 131970.KQ
-df = yf.download('005930.KS', start='2023-08-01', end='2024-01-07')
+df = yf.download(selected_code, start='2023-08-01', end='2024-01-07')
 
 # 마감가격
 fig1 = go.Figure(data = [go.Scatter(x=df.index, y=df['Close'], name = 'Close Price')], 
@@ -23,7 +35,6 @@ fig2 = go.Figure(data = [go.Scatter(x=df.index, y=df['Volume'], name = 'Volume')
                  layout=go.Layout(title='거래량', xaxis_title='날짜', yaxis_title='거래량'))
 
 ## 예측 그래프
-# df_prophet = df[['Date','Close']].rename(columns={'Date': 'ds', 'Close': 'y'})
 df_prophet = df.reset_index().rename(columns={'Date': 'ds', 'Close': 'y'})
 model = Prophet()
 model.fit(df_prophet)
@@ -34,20 +45,3 @@ fig1.add_trace(go.Scatter(x=forecast['ds'], y=forecast['yhat'], name='Predicted 
 
 st.plotly_chart(fig1)
 st.plotly_chart(fig2)
-
-## 주식정보 받아오기
-def get_krx_stock_codes():
-    url = 'http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13'
-    df = pd.read_html(url, header=0, encoding='CP949')[0]
-    df.종목코드 = df.종목코드.map('{:06d}'.format) + '.KS'  
-    return df
-
-df = get_krx_stock_codes()
-
-stocks = list()
-for stock in df['회사명']:
-    stocks.append(stock)
-    
-name = st.sidebar.selectbox('Name', df['종목코드'][df['회사명']==stocks])
-st.write(name)
-#############################################################################
